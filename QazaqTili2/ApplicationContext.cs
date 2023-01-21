4bccdd0000bbe2d44e7871f695e0ddbf295e1486
QaraByte <pchelpkz16@gmail.com>
2023-01-21 19:54:30 +0600

    Add project files.

---
+++ QazaqTili2/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QazaqTili2.Models;
+
+namespace QazaqTili2
+{
+    public class ApplicationContext
+    {
+        public DbSet<Word> Words => Set<Word>();
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=Qazaqtili;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ QazaqTili2/Models/Word.cs
@@ -0,0 +1,12 @@
+﻿namespace QazaqTili2.Models
+{
+    public class Word
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key, Column(Order = 0)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DefaultValue("getdate()")]
+        public DateTime CreateTime { get; set; }
+    }
+}
