05b18e10e0698eb8b88e83cce618900208f8486c
QaraByte <pchelpkz16@gmail.com>
2023-03-01 23:40:28 +0600

    1) Добавил AnalytController и вывел 10 последних добавленных слов; 2) +7 идей.

---
+++ QazaqTili2/Controllers/AnalyticsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QazaqTili2.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationContext _context;
+
+        public AnalyticsController(/*ILogger<AnalyticsController> logger,*/ ApplicationContext context)
+        {
+            //_logger = (ILogger<HomeController>?)logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var lastWords = _context.Words.Take(10).OrderByDescending(x => x.CreateTime).ToList();
+            return View(lastWords);
+        }
+    }
+}
